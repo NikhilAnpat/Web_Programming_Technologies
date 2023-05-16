7e43a79324d4ac23e50d8ca314e944e8412c00ca
NikhilAnpat <111105422+NikhilAnpat@users.noreply.github.com>
2023-05-16 07:28:50 -0700

    Add files via upload

---
+++ Java_script/Day__12/React/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default class Login extends React.Component
+{
+
+    render()
+    {
+        return <div id='d1'><form id='f1' >  
+       
+        <p>Username: <input type="text"></input></p>
+ 
+        <p>Password: <input type="password"></input></p>
+
+        <button>Login</button>
+        </form>
+        </div>
+        
+        
+        
+
+    }
+}
