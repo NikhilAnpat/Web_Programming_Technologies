aeb939e8eb36c7d50e70072977e660b6e59f4d1e
NikhilAnpat <111105422+NikhilAnpat@users.noreply.github.com>
2023-06-16 10:50:17 +0530

    Add files via upload

---
+++ Exam Wpt/Q3/StudentTable.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class StudentTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [
+        { name: "Swaraj", marks: 85 },
+        { name: "Abhi", marks: 92 },
+        { name: "Nik", marks: 76 },
+        { name: "Aditya", marks: 88 }
+      ]
+    };
+  }
+  
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Marks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.students.map((student, index) => (
+            <tr key={index}>
+              <td>{student.name}</td>
+              <td>{student.marks}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default StudentTable;
