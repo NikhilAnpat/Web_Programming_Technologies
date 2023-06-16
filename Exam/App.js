97f1cabc7c4ddd940d7d6f3763ab51bf300dec98
NikhilAnpat <111105422+NikhilAnpat@users.noreply.github.com>
2023-06-16 10:06:56 +0530

    Add files via upload

---
+++ Exam/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentTable from './StudentTable';
+
+
+
+function App() {
+  return (
+    <div>
+     <StudentTable></StudentTable>
+    </div>
+  );
+}
+
+export default App;
